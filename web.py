3dce0592594dc897dbe10f39709cc123e6b97236
Edmond Guo <edmondandy@gmail.com>
2025-06-29 13:43:06 +1000

    update

--- web.py
+++ web.py
@@ -12,8 +12,13 @@ st.title("My Todo App")
 st.header("This is my todo app.")
 st.write("This app is to increase your productivity by managing your tasks.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]  # Remove the checkbox state
+        st.rerun()
 
 st.text_input(label="Enter a new todo item", placeholder="e.g. Buy groceries", 
               on_change=add_todo, key="new_todo")
